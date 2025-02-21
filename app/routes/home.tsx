338a5fd257536e6300365db8a88069d682a67cf0
thedevbrian1 <brayomwas95@gmail.com>
2025-02-21 18:46:35 +0300

    Initial

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { Form, useLoaderData } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,6 +9,68 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+export async function loader({ request }) {
+  let choice = new URL(request.url).searchParams.get("choice");
+
+  let food = [
+    {
+      name: "Chocolate",
+      category: "Snacks",
+    },
+    {
+      name: "Tiramisu",
+      category: "Pastery",
+    },
+    {
+      name: "Cheese cake",
+      category: "Pastery",
+    },
+    {
+      name: "Coffee",
+      category: "Beverages",
+    },
+    {
+      name: "Soda",
+      category: "Beverages",
+    },
+  ];
+
+  // If one has selected a filter, return the filtered elements, else return all the food items
+  let filteredList;
+
+  if (choice) {
+    filteredList = food.filter((item) => item.category === choice);
+  } else {
+    filteredList = food;
+  }
+
+  return filteredList;
+}
+
 export default function Home() {
-  return <Welcome />;
+  let food = useLoaderData();
+  return (
+    <div className="p-6">
+      <h1>Restaurant</h1>
+      <Form>
+        <button className="bg-purple-500" name="choice" value="All">
+          All
+        </button>
+        <button className="bg-green-500" name="choice" value="Pastery">
+          Pastery
+        </button>
+        <button className="bg-orange-500" name="choice" value="Beverages">
+          Beverages
+        </button>
+        <button className="bg-white text-black" name="choice" value="Snacks">
+          Snacks
+        </button>
+      </Form>
+      <ul className="mt-8">
+        {food.map((item, index) => (
+          <li key={index}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
